1a6498dca51331c2b3dc27b716db61c5598b7d50
Scott DePouw <scott.depouw@nimblepros.com>
2023-10-10 08:12:32 -0400

    Messing around with using CancellationToken to marshall start/stoppage.

--- WorkServiceEight/Worker8.cs
+++ WorkServiceEight/Worker8.cs
@@ -3,31 +3,59 @@ using Microsoft.Extensions.Options;
 
 namespace WorkServiceEight;
 
-public class Worker8 : BackgroundService
+public class Worker8(ILogger<Worker8> logger, IOptions<MySettings> settings) : BackgroundService
 {
-    private readonly ILogger<Worker8> _logger;
-    private readonly MySettings _settings;
-
-    public Worker8(ILogger<Worker8> logger, IOptions<MySettings> settings)
-    {
-        _logger = logger;
-        _settings = settings.Value;
-    }
+    private readonly ILogger<Worker8> _logger = logger;
+    private readonly MySettings _settings = settings.Value;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        SomeProc proc1 = new("1");
+        SomeProc proc2 = new("2");
+        SomeProc proc3 = new("3");
+
+        stoppingToken.Register(() => proc1.Stop());
+        stoppingToken.Register(() => proc2.Stop());
+        stoppingToken.Register(() => proc3.Stop());
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker7 running at with version {version}", Environment.Version);
-            _logger.LogInformation("Settings! {Foo} | {Bar} | {Fizz} | {Buzz}",
-                _settings.Foo, _settings.Bar, _settings.Fizz, _settings.Buzz);
+            List<Task> tasks = new();
+            if (!proc1.DontStopMeNow) tasks.Add(proc1.Start());
+            if (!proc2.DontStopMeNow) tasks.Add(proc2.Start());
+            if (!proc3.DontStopMeNow) tasks.Add(proc3.Start());
+
+            await Task.WhenAll(tasks);
             await Task.Delay(1000, stoppingToken);
         }
     }
+}
 
-    public override Task StopAsync(CancellationToken cancellationToken)
+public class SomeProc(string name)
+{
+    public bool DontStopMeNow { get; private set; }
+    private string Name { get; set; } = name;
+
+    public async Task Start()
     {
-        _logger.LogInformation("StopAsync!");
-        return base.StopAsync(cancellationToken);
+        DontStopMeNow = true;
+        Console.WriteLine("Starting {0}. Don't stop me now!", Name);
+        for (int delay = 0; delay < 10; delay++)
+        {
+            Console.WriteLine("{0} working...", Name);
+            await Task.Delay(1000);
+        }
+        Console.WriteLine("{0} Done!", Name);
+        DontStopMeNow = false;
+    }
+
+    public async void Stop()
+    {
+        int delayCounter = 10;
+        while (DontStopMeNow && delayCounter > 0)
+        {
+            Console.WriteLine("Giving {0} a little more time... {1}", Name, delayCounter--);
+            await Task.Delay(1000);
+        }
+        Console.WriteLine("Stopping {0}!", Name);
     }
 }
