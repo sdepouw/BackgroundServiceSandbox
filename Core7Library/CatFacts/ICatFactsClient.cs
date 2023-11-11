018e1bc5e2f4fc9317dd6bf6e894f1d77d6bcdb7
Scott DePouw <scott.depouw@nimblepros.com>
2023-11-11 15:21:39 -0500

    notes

--- Core7Library/CatFacts/ICatFactsClient.cs
+++ Core7Library/CatFacts/ICatFactsClient.cs
@@ -4,6 +4,16 @@ namespace Core7Library.CatFacts;
 
 public interface ICatFactsClient
 {
+    /// <summary>
+    /// Refit notes:
+    /// - Always return <see cref="ApiResponse{T}"/> so that status code can be checked.
+    /// - Return type should always be marked as nullable (failed requests return null)
+    /// - Can make route Settings-injection capable by passing a string in for the whole route, or part of it
+    /// - Last parameter can be cancellation token and it'll work as expected
+    /// </summary>
+    /// <param name="theRoute">The route to call. Injected instead of hardcoded so that it can be configured</param>
+    /// <param name="cancellationToken">CancellationToken to respect.</param>
+    /// <returns></returns>
     [Get("/{theRoute}")]
     Task<ApiResponse<List<CatFact>?>> GetTheFactsAsync(string theRoute, CancellationToken cancellationToken);
 }
