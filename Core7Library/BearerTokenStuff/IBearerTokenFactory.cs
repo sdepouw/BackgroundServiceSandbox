e9ee6ee2b6230adc78fda16fc072ce8642612907
Scott DePouw <scott.depouw@nimblepros.com>
2023-11-12 11:54:21 -0500

    Documentation

--- Core7Library/BearerTokenStuff/IBearerTokenFactory.cs
+++ Core7Library/BearerTokenStuff/IBearerTokenFactory.cs
@@ -1,6 +1,18 @@
-﻿namespace Core7Library.BearerTokenStuff;
+﻿using Core7Library.Extensions;
 
+namespace Core7Library.BearerTokenStuff;
+
+/// <summary>
+/// Implement when using <see cref="ServiceCollectionExtensions.AddRefitClient{TClientInterface}"/> and
+/// setting "useAuthHeaderGetter" to true. That will use the implementation of this interface to get
+/// bearer tokens.
+/// </summary>
 public interface IBearerTokenFactory
 {
+    /// <summary>
+    /// Returns bearer token string to be added to Authorization headers.
+    /// Use: "Authorization: Bearer {token}"
+    /// </summary>
+    /// <returns>Token string to use in Authorization Bearer header</returns>
     Task<string> GetBearerTokenAsync(CancellationToken cancellationToken);
 }
