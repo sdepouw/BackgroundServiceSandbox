623743c07522bf0500a4e96fb61046440f27a4a1
Scott DePouw <scott.depouw@nimblepros.com>
2023-11-12 07:19:03 -0500

    JSON formatted for logging purposes

---
+++ Core7Library/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Core7Library.Extensions;
+
+public static class StringExtensions
+{
+    public static string? TryFormatJson(this string? json)
+    {
+        if (string.IsNullOrWhiteSpace(json)) return json;
+        try
+        {
+            var deserialized = JsonSerializer.Deserialize<dynamic>(json);
+            return JsonSerializer.Serialize(deserialized, new JsonSerializerOptions { WriteIndented = true });
+        }
+        catch (JsonException)
+        {
+            return json;
+        }
+    }
+}
---
+++ TestingTime/StringExtensionsTests/TryFormatJson.cs
@@ -0,0 +1,55 @@
+﻿using Core7Library.Extensions;
+using FluentAssertions;
+using Xunit;
+// ReSharper disable StringLiteralTypo
+
+namespace TestingTime.StringExtensionsTests;
+
+public class TryFormatJson
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ReturnsOriginalWhenEmpty(string emptyString)
+    {
+        string? formattedJson = emptyString.TryFormatJson();
+
+        formattedJson.Should().Be(emptyString);
+    }
+
+    [Fact]
+    public void ReturnsOriginalWhenNonJson()
+    {
+        const string nonJson = "Foo Bar Fizz Buzz";
+
+        string? formattedJson = nonJson.TryFormatJson();
+
+        formattedJson.Should().NotBeNull().And.Be(nonJson);
+    }
+
+    [Fact]
+    public void FormatsJson()
+    {
+        const string unformattedJson = @"{""foo"":""Bob"",""bar"":1,""fizz"":[{""ace"":""win"",""king"":""lose""},{""queen"":""better"",""jack"":""worse""}]}";
+        const string expectedFormattedJson =
+@"{
+  ""foo"": ""Bob"",
+  ""bar"": 1,
+  ""fizz"": [
+    {
+      ""ace"": ""win"",
+      ""king"": ""lose""
+    },
+    {
+      ""queen"": ""better"",
+      ""jack"": ""worse""
+    }
+  ]
+}";
+
+        string? formattedJson = unformattedJson.TryFormatJson();
+
+        formattedJson.Should().NotBeNull().And.Be(expectedFormattedJson);
+    }
+}
