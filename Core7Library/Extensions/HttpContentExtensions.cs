5e30ab5c70ec541f4e9d9a90f282f082c615a0a5
Scott DePouw <scott.depouw@nimblepros.com>
2023-11-12 08:07:59 -0500

    More fiddling with logging

---
+++ Core7Library/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+
+namespace Core7Library.Extensions;
+
+public static class HttpContentExtensions
+{
+    private static readonly string[] TextBasedTypes = {"html", "text", "xml", "json", "txt", "x-www-form-urlencoded"};
+
+    public static bool ContentIsTextBased(this HttpRequestMessage request) => request.Content.ContentIsTextBased();
+    public static bool ContentIsTextBased(this HttpResponseMessage response) => response.Content.ContentIsTextBased();
+
+    private static bool ContentIsTextBased(this HttpContent? content)
+    {
+        if (content is null) return false;
+        return content is StringContent || ContentIsTextBased(content.Headers) || ContentIsTextBased(content.Headers);
+    }
+
+    private static bool ContentIsTextBased(this HttpHeaders headers)
+    {
+        if (!headers.TryGetValues("Content-Type", out IEnumerable<string>? values))
+        {
+            return false;
+        }
+        var header = string.Join(" ", values);
+        return TextBasedTypes.Any(t => header.Contains(t, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
--- Core7Library/HttpLoggingHandler.cs
+++ Core7Library/HttpLoggingHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Net.Http.Headers;
 using Core7Library.Extensions;
 using Microsoft.Extensions.Logging;
 
@@ -7,6 +6,7 @@ namespace Core7Library;
 
 public class HttpLoggingHandler : DelegatingHandler
 {
+    private static readonly Stopwatch Stopwatch = new();
     private readonly ILogger<HttpLoggingHandler> _logger;
 
     public HttpLoggingHandler(ILogger<HttpLoggingHandler> logger)
@@ -16,43 +16,24 @@ public class HttpLoggingHandler : DelegatingHandler
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        _logger.LogDebug("{Request}", request);
-        if (IsTextBasedContentType(request.Content))
+        Guid id = Guid.NewGuid();
+        _logger.LogDebug("[{Id}] Request: {Request}", id, request);
+        if (request.ContentIsTextBased())
         {
             var requestContent = await request.Content!.ReadAsStringAsync(cancellationToken);
-            _logger.LogDebug("Content: {RequestContent}", requestContent.TryFormatJson());
+            _logger.LogDebug("[{Id}] Content: {RequestContent}", id, requestContent.TryFormatJson());
         }
-
-        var stopwatch = new Stopwatch();
-        stopwatch.Start();
+        Stopwatch.Restart();
         var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-        stopwatch.Stop();
-        _logger.LogDebug("Request Duration: {RequestDuration}", stopwatch.Elapsed);
-        _logger.LogDebug("{Response}", response);
-        if (IsTextBasedContentType(response.Content))
+        Stopwatch.Stop();
+        _logger.LogDebug("[{Id}] Response [{RequestDuration}]: {Response}", id, Stopwatch.Elapsed, response);
+        if (response.ContentIsTextBased())
         {
-            stopwatch.Restart();
+            Stopwatch.Restart();
             var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
-            stopwatch.Stop();
-            _logger.LogDebug("Response Read Duration: {ResponseReadDuration}", stopwatch.Elapsed);
-            _logger.LogDebug("Response Content: {ResponseContent}", responseContent.TryFormatJson());
+            Stopwatch.Stop();
+            _logger.LogDebug("[{Id}] Response Content [{ResponseReadDuration}]: {ResponseContent}", id, Stopwatch.Elapsed, responseContent.TryFormatJson());
         }
         return response;
-    }
-
-    private readonly string[] _types = {"html", "text", "xml", "json", "txt", "x-www-form-urlencoded"};
-    private bool IsTextBasedContentType(HttpContent? content)
-    {
-        if (content is null) return false;
-        return content is StringContent || IsTextBasedContentType(content.Headers) || IsTextBasedContentType(content.Headers);
-    }
-
-    private bool IsTextBasedContentType(HttpHeaders headers)
-    {
-        if (!headers.TryGetValues("Content-Type", out IEnumerable<string>? values))
-            return false;
-        var header = string.Join(" ", values).ToLowerInvariant();
-
-        return _types.Any(t => header.Contains(t));
     }
 }
