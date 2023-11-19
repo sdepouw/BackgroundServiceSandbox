0996ca6ac854820c5c95ab94f54a8c69a9669e96
Scott DePouw <scott.depouw@nimblepros.com>
2023-11-19 08:23:13 -0500

    Autofac module for Core7Library

---
+++ Core7Library/Core7LibraryAutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Core7Library.BearerTokenStuff;
+using Core7Library.CatFacts;
+
+namespace Core7Library;
+
+public class Core7LibraryAutofacModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<CatFactsClientService>().As<ICatFactsClientService>();
+        builder.RegisterType<OAuthClientService>().As<IOAuthClientService>();
+    }
+}
