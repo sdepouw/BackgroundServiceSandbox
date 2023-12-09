fabd64e491bd85150f0027f3d6a4fc2a26857525
Scott DePouw <scott.depouw@nimblepros.com>
2023-12-08 21:02:55 -0500

    For really real

--- Core8WebAPI/Program.cs
+++ Core8WebAPI/Program.cs
@@ -9,5 +9,6 @@ var app = builder.BuildAndValidate();
 app.MapGet("/", () => "Try going to /foo/whatever! Or /bar if you have nothing better to do for a while!");
 app.MapGet("/foo/{myVal}", SomeClass.DoTheNeedful);
 app.MapGet("/bar", SomeClass.TestingDI);
+app.MapGet("/kaboom", () => { throw new NotImplementedException("Nobody here but us trees!"); });
 
 app.Run();
