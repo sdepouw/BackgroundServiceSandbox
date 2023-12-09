ba0d3025b11c9828517495ab4f8918c4d573920f
Scott DePouw <scott.depouw@nimblepros.com>
2023-12-08 20:40:03 -0500

    Messin' with MiminalAPIs

--- Core8WebAPI/Program.cs
+++ Core8WebAPI/Program.cs
@@ -1,12 +1,20 @@
 using Core8Library.SuperBuilder;
 using Core8WebAPI;
+using Microsoft.Extensions.Options;
 
 SuperWebApplicationBuilder builder = SuperWebApplicationBuilder.Create();
 builder.WithSettings<ExampleSettings>();
 
-// TODO: Shouldn't have to cast this BuildAndValidate(). It's the only missing part of the grand plan.
 var app = builder.BuildAndValidate();
 
-app.MapGet("/", async (IExampleHandler dep, CancellationToken token) => await dep.GimmeAsync(token));
+app.MapGet("/", () => "Try going to /foo/whatever!");
+app.MapGet("/foo/{myVal}", DoTheNeedful);
 
 app.Run();
+
+static async Task<IResult> DoTheNeedful(string? myVal, IOptions<ExampleSettings> settings, IExampleHandler dep, CancellationToken token)
+{
+    if (myVal == "aaa") return TypedResults.BadRequest();
+    await dep.GimmeAsync(token);
+    return TypedResults.Ok(settings.Value);
+}
