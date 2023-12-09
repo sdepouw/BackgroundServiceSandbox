808aaa192ef2a2b037e4b5265d5901dc4d8fcba7
Scott DePouw <scott.depouw@nimblepros.com>
2023-12-09 07:26:08 -0500

    Testing Endpoint Filters

---
+++ TestingTime/EndpointFilterTests/APIGuardFilterTests.cs
@@ -0,0 +1,57 @@
+﻿using Core8WebAPI;
+using Core8WebAPI.Filters;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace TestingTime.EndpointFilterTests;
+
+public class APIGuardFilterTests : EndpointFilterTestBase
+{
+    private readonly ExampleSettings _settings = new() { APISecretKey = "def-456" };
+    private readonly APIGuardFilter _filter;
+    protected override IEndpointFilter EndpointFilter => _filter;
+
+    public APIGuardFilterTests() => _filter = new APIGuardFilter(Options.Create(_settings));
+
+    [Fact]
+    public async Task ContinuesWhenAPIKeyMatches()
+    {
+        AppendRequestHeader("Secret", _settings.APISecretKey);
+
+        var result = await InvokeEndpointFilterAsync();
+
+        EndpointFilterContinued(result).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ReturnsNotFoundForMissingHeader()
+    {
+        IResult expectedResult = Results.NotFound();
+
+        var result = await InvokeEndpointFilterAsync();
+
+        using (new AssertionScope())
+        {
+            result.Should().Be(expectedResult);
+            EndpointFilterContinued(result).Should().BeFalse();
+        }
+    }
+
+    [Fact]
+    public async Task ReturnsNotFoundForHeaderWithWrongAPIKey()
+    {
+        IResult expectedResult = Results.NotFound();
+        AppendRequestHeader("Secret", "incorrect-api-key");
+
+        var result = await InvokeEndpointFilterAsync();
+
+        using (new AssertionScope())
+        {
+            result.Should().Be(expectedResult);
+            EndpointFilterContinued(result).Should().BeFalse();
+        }
+    }
+}
---
+++ TestingTime/EndpointFilterTests/EndpointFilterTestBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace TestingTime.EndpointFilterTests;
+
+public abstract class EndpointFilterTestBase
+{
+    /// <summary>
+    /// The <see cref="IEndpointFilter"/> under test
+    /// </summary>
+    protected abstract IEndpointFilter EndpointFilter { get; }
+    private readonly object? _nextReturnValue = Guid.NewGuid().ToString();
+    private readonly HttpContext _httpContext = new DefaultHttpContext();
+
+    /// <summary>
+    /// Appends a given key/value pair as a header to the <see cref="HttpContext"/> that will
+    /// get passed to the <see cref="IEndpointFilter"/> under test
+    /// </summary>
+    protected void AppendRequestHeader(string key, StringValues value)
+    {
+        _httpContext.Request.Headers.Append(key, value);
+    }
+
+    /// <summary>
+    /// Calls <see cref="IEndpointFilter.InvokeAsync"/>
+    /// </summary>
+    protected ValueTask<object?> InvokeEndpointFilterAsync()
+    {
+        DefaultEndpointFilterInvocationContext endpointFilterContext = new(_httpContext);
+        ValueTask<object?> Next(EndpointFilterInvocationContext _) => ValueTask.FromResult(_nextReturnValue);
+        return EndpointFilter.InvokeAsync(endpointFilterContext, Next);
+    }
+
+    /// <summary>
+    /// Returns true when the <see cref="IEndpointFilter"/> under test called "await next(context)"
+    /// instead of returning a different result (e.g. <see cref="Results.NotFound"/>);
+    /// useful for testing whether or not the <see cref="IEndpointFilter"/> returned a happy path scenario
+    /// </summary>
+    protected bool EndpointFilterContinued(object? returnedResult) => returnedResult == _nextReturnValue;
+}
